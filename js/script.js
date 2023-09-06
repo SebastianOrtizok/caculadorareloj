8f247721537cd1c1c203e33b1ddfdf9267a187e4
“Sebastian <impactotenisok@gmail.com>
2023-09-06 15:47:16 -0300

    Se agregó sonido y comienzo de la funcion hora cronometro fecha

--- js/script.js
+++ js/script.js
@@ -1,68 +1,86 @@
 const numberButtons = document.querySelectorAll(".botones");
-const operaciones=document.querySelectorAll(".operacion");
-const visor=document.querySelector(".visor");
-const calcular=document.querySelector(".calcular");
-let number1=0;
-let number2=0;
+const operaciones = document.querySelectorAll(".operacion");
+const visor = document.querySelector(".visor");
+const calcular = document.querySelector(".calcular");
+const audioElement = document.getElementById("bip");
+
+let number1 = 0;
+let number2 = 0;
 let operacion;
 let resultado;
-let borrarpantalla="false"
+let borrarpantalla = "false";
+let mode=0;
 
+opcion()
+
+function opcion(){
+    if (mode==0){
+        visor.innerHTML = "HORA";
+    }
+    if (mode==1){
+        visor.innerHTML = "0";
+    }
+    if (mode==2){
+        visor.innerHTML = "CRONOMETRO";
+    }
+    if (mode==3){
+        visor.innerHTML = "FECHA";
+        mode=0
+    }
+}
 
 // Agrega el evento de clic a cada botón
-numberButtons.forEach(Element => {
-    Element.addEventListener("click", function() {
-        if (borrarpantalla=="true"){
-            visor.innerHTML=""
-        }
-        borrarpantalla="false"
-        const number = Element.textContent;
-        visor.innerHTML+=number    
-    });
+numberButtons.forEach((Element) => {
+	Element.addEventListener("click", function () {
+		if (borrarpantalla == "true") {
+			visor.innerHTML = "";
+		}
+		borrarpantalla = "false";
+		const number = Element.textContent;
+		visor.innerHTML += number;
+		audioElement.play();
+	});
+});
+
+operaciones.forEach((Element) => {
+	Element.addEventListener("click", function () {
+		const operaciones = Element.textContent;
+		number1 = parseFloat(visor.innerHTML);
+		borrarpantalla = "true";
+		operacion = operaciones;
+        audioElement.play();
+		if (operacion == "Borrar") {
+			visor.innerHTML = "0";
+			number1 = 0;
+			number2 = 0;
+            mode+=1;
+			opcion()
+
+		}
+	});
 });
 
 
-operaciones.forEach(Element =>{
-    Element.addEventListener("click", function(){
-        const operaciones = Element.textContent;
-        number1=parseFloat(visor.innerHTML);
-        borrarpantalla="true"
-        operacion=operaciones
-        if (operacion== "Borrar"){
-            visor.innerHTML=""
-            number1=0;
-            number2=0;
-            console.log(operacion)
-        }
-    })
-})
-
-calcular.addEventListener("click", function(){
-    number2=parseFloat(visor.innerHTML);
-    if (isNaN(number1) || isNaN(number2)) {
-        visor.innerHTML=""
-        number1=0;
-        number2=0; 
-    }else {
-
-    if (operacion== "+"){
-        resultado= number1 + number2
-    }
-    if (operacion== "-"){
-        resultado= number1 - number2
-    }
-    if (operacion== "*"){
-        resultado= number1 * number2
-    }
-    if (operacion== "/"){
-        resultado= number1 / number2
-    }
-
-    visor.innerHTML=resultado
-}
-}) 
-
-
-
-
-
+calcular.addEventListener("click", function () {
+	number2 = parseFloat(visor.innerHTML);
+	if (isNaN(number1) || isNaN(number2)) {
+		visor.innerHTML = "";
+		number1 = 0;
+		number2 = 0;
+	} else {
+		if (operacion == "+") {
+			resultado = number1 + number2;
+		}
+		if (operacion == "-") {
+			resultado = number1 - number2;
+		}
+		if (operacion == "*") {
+			resultado = number1 * number2;
+		}
+		if (operacion == "/") {
+			resultado = number1 / number2;
+		}
+        audioElement.play();
+		visor.innerHTML = resultado;
+	}
+});
